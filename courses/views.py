8dd8b7d7e58c06ee313ebf43c9382850855ec046
taher <tasd871@gmail.com>
2022-01-12 14:48:33 +0330

    viewsets and routers

--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import get_object_or_404
 #from django.shortcuts import render  because we dont need rendering in rest
 from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 from . import models
 from . import serializers
+
 
 class ListCreateCourse(generics.ListCreateAPIView):
     queryset = models.Course.objects.all()
@@ -12,6 +17,8 @@ class ListCreateCourse(generics.ListCreateAPIView):
 class RetriveUpdateDestroyCourse(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
+
+
 
 
 class ListCreateReview(generics.ListCreateAPIView):
@@ -25,11 +32,26 @@ class ListCreateReview(generics.ListCreateAPIView):
         serializer.save(course=course)
           
 
-
 class RetriveUpdateDestroyReview(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Review.objects.all()
     serializer_class = serializers.ReviewSerializer
 
     def get_object(self):
         return get_object_or_404(self.get_queryset(),course_id=self.kwargs.get('course_pk'),pk=self.kwargs.get('pk'))
-        
+
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
+
+    @action(methods=['get'],detail=True)
+    def reviews(self,request,pk=None):
+        course = self.get_object()
+        Serializer = serializers.ReviewSerializer(course.reviews.all(), many=True)
+        return Response(Serializer.data)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
